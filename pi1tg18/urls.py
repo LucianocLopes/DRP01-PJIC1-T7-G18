86a1e244d238ac55b6924a55900f97f395865228
LucianoClopes <luciano_clopes@hotmail.com>
2024-03-23 23:10:51 -0300

    preparing static file folder and template with html test

--- pi1tg18/urls.py
+++ pi1tg18/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="base.html"), name="index"),
     path('admin/', admin.site.urls),
 ]
 
