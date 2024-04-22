5eed965ffaa0e481bfd43ab8866954d4dfb95e75
LucianoCLopes <luciano_clopes@hotmail.com>
2024-04-22 00:36:37 -0300

    add app discipline and configuration

---
+++ discipline/views.py
@@ -0,0 +1,75 @@
+from django.views import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+from .models import Discipline, Graduation
+
+from .forms import DisciplineForm, GraduationForm
+
+
+class DisciplineBaseView(PermissionRequiredMixin, View):
+    model = Discipline
+    templatename = 'discipline/discipline_list.html'
+    success_url = reverse_lazy('discipline_all')
+    form_class = DisciplineForm
+
+
+class DisciplineListView(DisciplineBaseView, ListView):
+    "list view"
+    permission_required = 'discipline.view_discipline'
+
+
+class DisciplineDetailView(DisciplineBaseView, DetailView):
+    'detailview'
+    permission_required = 'discipline.change_discipline'
+
+
+class DisciplineCreateView(DisciplineBaseView, CreateView):
+    'createview'
+    permission_required = 'discipline.add_discipline'
+
+
+class DisciplineUpdateView(DisciplineBaseView, UpdateView):
+    'updadeview'
+    permission_required = 'discipline.change_discipline'
+
+
+class DisciplineDeleteView(DisciplineBaseView, DeleteView):
+    'deleteview'
+    permission_required = 'discipline.delete_discipline'
+
+
+class GraduationBaseView(PermissionRequiredMixin, View):
+    model = Graduation
+    templatename = 'graduation/graduation_list.html'
+    success_url = reverse_lazy('graduation_all')
+    form_class = GraduationForm
+
+
+class GraduationListView(GraduationBaseView, ListView):
+    "list view"
+    permission_required = 'graduation.view_graduation'
+
+
+class GraduationDetailView(GraduationBaseView, DetailView):
+    'detailview'
+    permission_required = 'graduation.change_graduation'
+
+
+class GraduationCreateView(GraduationBaseView, CreateView):
+    'createview'
+    permission_required = 'graduation.add_graduation'
+
+
+class GraduationUpdateView(GraduationBaseView, UpdateView):
+    'updadeview'
+    permission_required = 'graduation.change_graduation'
+
+
+class GraduationDeleteView(GraduationBaseView, DeleteView):
+    'deleteview'
+    permission_required = 'graduation.delete_graduation'
--- pi1tg18/settings/base.py
+++ pi1tg18/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'school',
     'teacher',
     'student',
+    'discipline',
 ]
 
 MIDDLEWARE = [
