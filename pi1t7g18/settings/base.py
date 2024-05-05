584f97934faebd6bd4c53942b571b0f518711f46
LucianoCLopes <luciano_clopes@hotmail.com>
2024-05-05 15:34:33 -0300

    delete itens

--- pi1t7g18/settings/base.py
+++ pi1t7g18/settings/base.py
@@ -20,8 +20,11 @@ from pathlib import Path
 from django.contrib.messages import constants
 
 <<<<<<< HEAD:pi1t7g18/settings/base.py
+<<<<<<< HEAD:pi1t7g18/settings/base.py
 =======
 import dj_database_url
+=======
+>>>>>>> 3a9bd23 (delete itens):pi1tg18/settings/base.py
 
 >>>>>>> 01b6098 (new corrections and mysql configurations):pi1tg18/settings/base.py
 env = Env(
